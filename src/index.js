f2f4319c3a5e8c2a7323e71ab44ce02c1c5c5b26
ndk1195 <ndkhoa1195@gmail.com>
2024-12-30 15:02:25 +0700

    finish project

---
+++ src/index.js
@@ -0,0 +1,79 @@
+const data = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
+
+const chart = document.getElementById("chart");
+
+const maxAmount = Math.max(...data.map((item) => item.amount));
+
+data.forEach((item) => {
+  const bar = document.createElement("div");
+
+  bar.classList.add(
+    "relative",
+    "flex",
+    "w-full",
+    "flex-col",
+    "items-center",
+    "gap-[11px]",
+    "sm:max-w-[50.365px]",
+    "sm:gap-2",
+    "group",
+    "cursor-pointer",
+    "justify-end",
+  );
+
+  const height = ((item.amount / maxAmount) * 100 * 150) / 100;
+
+  bar.innerHTML = `
+            <div
+              class="relative w-full rounded-[3px] bg-soft-red sm:rounded-[5px]"
+              style='height: ${height}px'>
+              <!-- tooltip -->
+              <div
+                class="absolute -top-12 left-1/2 hidden -translate-x-1/2 rounded-[3px] bg-dark-brown p-2 text-[15px] font-bold leading-none text-very-pale-orange group-hover:block sm:rounded-[5px] sm:text-lg"
+              >
+                $${item.amount}
+              </div>
+              <!-- tooltip -->
+            </div>
+            <p class="text-xs leading-none text-medium-brown sm:text-[15px]">
+              ${item.day}
+            </p>
+            `;
+
+  if (item.amount === maxAmount) {
+    bar.children[0].classList.add("bg-cyan");
+  } else {
+    bar.children[0].classList.add("bg-soft-red");
+  }
+
+  chart.appendChild(bar);
+});
